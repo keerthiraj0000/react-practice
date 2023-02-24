fd6fab6cdddaf5dd892db2e9767122cf5f258a71
keerthiraj0000 <keerthirajml@gmail.com>
2023-02-24 12:47:28 +0530

    aooooo

---
+++ Html/index.js
@@ -0,0 +1,13 @@
+console.log(5+3);
+console.log("5"-2);
+console.log("a" - 4);
+console.log("abc" + 7);
+var str="hello"
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.charAt(2));
+var name="raj";
+console.log(name);
+let id=10;
+console.log(id);
